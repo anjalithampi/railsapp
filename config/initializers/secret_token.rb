52d7194ad2333f40ef70e22e1862831db16a213f
Anjali Thampi <anjalithampi1982@gmail.com>
2012-09-02 14:51:01 +0530

    Adding railsapp   *  Created new rails app   *  Adding gitignore

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Railsapp::Application.config.secret_token = 'e15fef3235765ff885016474d6569c823714da8489586599cee47ac52470f1ee2780670a0118a03b341a53cbe6bb8b5b7947bfd337afdc4514b5e8e71b32f1c2'
